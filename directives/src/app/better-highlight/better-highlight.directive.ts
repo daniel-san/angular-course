ae12bfddd02d9af8fa822609b0ed6128a224f2b0
Daniel Lucas <dlsbdaniel@gmail.com>
2019-02-14 11:22:18 -0300

    Using HostListener decorator to listen to events and change the DOM

--- directives/src/app/better-highlight/better-highlight.directive.ts
+++ directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,6 +8,15 @@ export class BetterHighlightDirective implements OnInit{
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.renderer.setStyle(this.elRef.nativeElement, 'transition', '0.2s');
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
   }
 }
